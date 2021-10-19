fb5679f3824be926407fa606f036a8d16e749d1f
Alliion <kruzina.alina@gmail.com>
2021-10-19 23:07:54 +0300

    Вроде рабочее..

--- main.py
+++ main.py
@@ -16,34 +16,74 @@ WORDS = [
     'guide', 'agreement', 'tosha', 'alina'
 ]
 
-class Hangman():
+
+def is_invalid_letter(input_):
+    return input_.isdigit() or (input_.isalpha() and len(input_) > 1)
+
+
+class Hangman:
 
     def __init__(self, word_to_guess):
-
+        self.failed_attempts = 0
+        self.word_to_guess = word_to_guess
+        self.game_progress = list('_' * len(self.word_to_guess))
 
     def find_indexes(self, letter):
+        return [i for i, char in enumerate(self.word_to_guess) if letter == char]
 
 
-    def is_invalid_letter(self, input_):
+def get_user_input():
+    user_input = input('\nPlease type a letter: ')
+    return user_input
 
 
-class GameProcess():
+class GameProcess:
+
+    def print_game_status(self):
+        print('\n')
+        print('\n'.join(HANGMAN[:self.failed_attempts]))
+        print('\n')
+        print(' '.join(self.game_progress))
 
     def update_progress(self, letter, indexes):
-
-
-    def get_user_input(self):
+        for index in indexes:
+            self.game_progress[index] = letter
 
 
 class Play(Hangman, GameProcess):
 
     def play(self):
+        while self.failed_attempts < len(HANGMAN):
+            self.print_game_status()
+            user_input = get_user_input()
+
+            # Validate the user input
+            if self.is_invalid_letter(user_input):
+                print('¡The input is not a letter!')
+                continue
+            # Check if the letter is not already guessed
+            if user_input in self.game_progress:
+                print('You already have guessed that letter')
+                continue
+
+            if user_input in self.word_to_guess:
+                indexes = self.find_indexes(user_input)
+                self.update_progress(user_input, indexes)
+                # If there is no letter to find in the word
+                if self.game_progress.count('_') == 0:
+                    print('\n¡Yay! You win!')
+                    print('The word is: {0}'.format(self.word_to_guess))
+                    quit()
+            else:
+                self.failed_attempts += 1
+        print("\n¡OMG! You lost!")
 
 
 if __name__ == '__main__':
     word_to_guess = random.choise(WORDS)
     hangman = Play(word_to_guess)
     hangman.play()
+
 
 class Tests(unittest.TestCase):
 
