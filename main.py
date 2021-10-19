05928ef1ebca0d6b6ac68648f9c252731dce73e5
Alliion <kruzina.alina@gmail.com>
2021-10-19 22:29:14 +0300

    Структура проекта

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+import random
+
+HANGMAN = [
+    '________',
+    '|       |',
+    '|       O',
+    '|       |',
+    '|      /|\ ',
+    '|       |',
+    '|      / \ '
+]
+
+WORDS = [
+    'mistake', 'vegas', 'liberty', 'depth',
+    'guide', 'agreement', 'tosha', 'alina'
+]
+
+class Hangman():
+
+    def __init__(self, word_to_guess):
+
+
+    def find_indexes(self, letter):
+
+
+    def is_invalid_letter(self, input_):
+
+
+class GameProcess():
+
+    def update_progress(self, letter, indexes):
+
+
+    def get_user_input(self):
+
+
+class Play(Hangman, GameProcess):
+
+    def play(self):
+
 
